4acf6139fa35a34c974647d63711cf376dc698e0
poyu39 <bruce920309@gmail.com>
2024-08-03 14:10:10 +0800

    style: file format

--- tDCF_python/eval_metrics.py
+++ tDCF_python/eval_metrics.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 
+
 def obtain_asv_error_rates(tar_asv, non_asv, spoof_asv, asv_threshold):
 
     # False alarm and miss rates for ASV
@@ -20,25 +21,35 @@ def compute_det_curve(target_scores, nontarget_scores):
 
     n_scores = target_scores.size + nontarget_scores.size
     all_scores = np.concatenate((target_scores, nontarget_scores))
-    labels = np.concatenate((np.ones(target_scores.size), np.zeros(nontarget_scores.size)))
+    labels = np.concatenate(
+        (np.ones(target_scores.size), np.zeros(nontarget_scores.size))
+    )
 
     # Sort labels based on scores
-    indices = np.argsort(all_scores, kind='mergesort')
+    indices = np.argsort(all_scores, kind="mergesort")
     labels = labels[indices]
 
     # Compute false rejection and false acceptance rates
     tar_trial_sums = np.cumsum(labels)
-    nontarget_trial_sums = nontarget_scores.size - (np.arange(1, n_scores + 1) - tar_trial_sums)
+    nontarget_trial_sums = nontarget_scores.size - (
+        np.arange(1, n_scores + 1) - tar_trial_sums
+    )
 
-    frr = np.concatenate((np.atleast_1d(0), tar_trial_sums / target_scores.size))  # false rejection rates
-    far = np.concatenate((np.atleast_1d(1), nontarget_trial_sums / nontarget_scores.size))  # false acceptance rates
-    thresholds = np.concatenate((np.atleast_1d(all_scores[indices[0]] - 0.001), all_scores[indices]))  # Thresholds are the sorted scores
+    frr = np.concatenate(
+        (np.atleast_1d(0), tar_trial_sums / target_scores.size)
+    )  # false rejection rates
+    far = np.concatenate(
+        (np.atleast_1d(1), nontarget_trial_sums / nontarget_scores.size)
+    )  # false acceptance rates
+    thresholds = np.concatenate(
+        (np.atleast_1d(all_scores[indices[0]] - 0.001), all_scores[indices])
+    )  # Thresholds are the sorted scores
 
     return frr, far, thresholds
 
 
 def compute_eer(target_scores, nontarget_scores):
-    """ Returns equal error rate (EER) and the corresponding threshold. """
+    """Returns equal error rate (EER) and the corresponding threshold."""
     frr, far, thresholds = compute_det_curve(target_scores, nontarget_scores)
     abs_diffs = np.abs(frr - far)
     min_index = np.argmin(abs_diffs)
@@ -46,7 +57,15 @@ def compute_eer(target_scores, nontarget_scores):
     return eer, thresholds[min_index]
 
 
-def compute_tDCF(bonafide_score_cm, spoof_score_cm, Pfa_asv, Pmiss_asv, Pmiss_spoof_asv, cost_model, print_cost):
+def compute_tDCF(
+    bonafide_score_cm,
+    spoof_score_cm,
+    Pfa_asv,
+    Pmiss_asv,
+    Pmiss_spoof_asv,
+    cost_model,
+    print_cost,
+):
     """
     Compute Tandem Detection Cost Function (t-DCF) [1] for a fixed ASV system.
     In brief, t-DCF returns a detection cost of a cascaded system of this form,
@@ -129,41 +148,60 @@ def compute_tDCF(bonafide_score_cm, spoof_score_cm, Pfa_asv, Pmiss_asv, Pmiss_sp
           TODO: <add link>
     """
 
-
     # Sanity check of cost parameters
-    if cost_model['Cfa_asv'] < 0 or cost_model['Cmiss_asv'] < 0 or \
-            cost_model['Cfa_cm'] < 0 or cost_model['Cmiss_cm'] < 0:
-        print('WARNING: Usually the cost values should be positive!')
+    if (
+        cost_model["Cfa_asv"] < 0
+        or cost_model["Cmiss_asv"] < 0
+        or cost_model["Cfa_cm"] < 0
+        or cost_model["Cmiss_cm"] < 0
+    ):
+        print("WARNING: Usually the cost values should be positive!")
 
-    if cost_model['Ptar'] < 0 or cost_model['Pnon'] < 0 or cost_model['Pspoof'] < 0 or \
-            np.abs(cost_model['Ptar'] + cost_model['Pnon'] + cost_model['Pspoof'] - 1) > 1e-10:
-        sys.exit('ERROR: Your prior probabilities should be positive and sum up to one.')
+    if (
+        cost_model["Ptar"] < 0
+        or cost_model["Pnon"] < 0
+        or cost_model["Pspoof"] < 0
+        or np.abs(cost_model["Ptar"] + cost_model["Pnon"] + cost_model["Pspoof"] - 1)
+        > 1e-10
+    ):
+        sys.exit(
+            "ERROR: Your prior probabilities should be positive and sum up to one."
+        )
 
     # Unless we evaluate worst-case model, we need to have some spoof tests against asv
     if Pmiss_spoof_asv is None:
-        sys.exit('ERROR: you should provide miss rate of spoof tests against your ASV system.')
+        sys.exit(
+            "ERROR: you should provide miss rate of spoof tests against your ASV system."
+        )
 
     # Sanity check of scores
     combined_scores = np.concatenate((bonafide_score_cm, spoof_score_cm))
     if np.isnan(combined_scores).any() or np.isinf(combined_scores).any():
-        sys.exit('ERROR: Your scores contain nan or inf.')
+        sys.exit("ERROR: Your scores contain nan or inf.")
 
     # Sanity check that inputs are scores and not decisions
     n_uniq = np.unique(combined_scores).size
     if n_uniq < 3:
-        sys.exit('ERROR: You should provide soft CM scores - not binary decisions')
+        sys.exit("ERROR: You should provide soft CM scores - not binary decisions")
 
     # Obtain miss and false alarm rates of CM
-    Pmiss_cm, Pfa_cm, CM_thresholds = compute_det_curve(bonafide_score_cm, spoof_score_cm)
+    Pmiss_cm, Pfa_cm, CM_thresholds = compute_det_curve(
+        bonafide_score_cm, spoof_score_cm
+    )
 
     # Constants - see ASVspoof 2019 evaluation plan
-    C1 = cost_model['Ptar'] * (cost_model['Cmiss_cm'] - cost_model['Cmiss_asv'] * Pmiss_asv) - \
-         cost_model['Pnon'] * cost_model['Cfa_asv'] * Pfa_asv
-    C2 = cost_model['Cfa_cm'] * cost_model['Pspoof'] * (1 - Pmiss_spoof_asv)
+    C1 = (
+        cost_model["Ptar"]
+        * (cost_model["Cmiss_cm"] - cost_model["Cmiss_asv"] * Pmiss_asv)
+        - cost_model["Pnon"] * cost_model["Cfa_asv"] * Pfa_asv
+    )
+    C2 = cost_model["Cfa_cm"] * cost_model["Pspoof"] * (1 - Pmiss_spoof_asv)
 
     # Sanity check of the weights
     if C1 < 0 or C2 < 0:
-        sys.exit('You should never see this error but I cannot evalute tDCF with negative weights - please check whether your ASV error rates are correctly computed?')
+        sys.exit(
+            "You should never see this error but I cannot evalute tDCF with negative weights - please check whether your ASV error rates are correctly computed?"
+        )
 
     # Obtain t-DCF curve for all thresholds
     tDCF = C1 * Pmiss_cm + C2 * Pfa_cm
@@ -174,20 +212,58 @@ def compute_tDCF(bonafide_score_cm, spoof_score_cm, Pfa_asv, Pmiss_asv, Pmiss_sp
     # Everything should be fine if reaching here.
     if print_cost:
 
-        print('t-DCF evaluation from [Nbona={}, Nspoof={}] trials\n'.format(bonafide_score_cm.size, spoof_score_cm.size))
-        print('t-DCF MODEL')
-        print('   Ptar         = {:8.5f} (Prior probability of target user)'.format(cost_model['Ptar']))
-        print('   Pnon         = {:8.5f} (Prior probability of nontarget user)'.format(cost_model['Pnon']))
-        print('   Pspoof       = {:8.5f} (Prior probability of spoofing attack)'.format(cost_model['Pspoof']))
-        print('   Cfa_asv      = {:8.5f} (Cost of ASV falsely accepting a nontarget)'.format(cost_model['Cfa_asv']))
-        print('   Cmiss_asv    = {:8.5f} (Cost of ASV falsely rejecting target speaker)'.format(cost_model['Cmiss_asv']))
-        print('   Cfa_cm       = {:8.5f} (Cost of CM falsely passing a spoof to ASV system)'.format(cost_model['Cfa_cm']))
-        print('   Cmiss_cm     = {:8.5f} (Cost of CM falsely blocking target utterance which never reaches ASV)'.format(cost_model['Cmiss_cm']))
-        print('\n   Implied normalized t-DCF function (depends on t-DCF parameters and ASV errors), s=CM threshold)')
+        print(
+            "t-DCF evaluation from [Nbona={}, Nspoof={}] trials\n".format(
+                bonafide_score_cm.size, spoof_score_cm.size
+            )
+        )
+        print("t-DCF MODEL")
+        print(
+            "   Ptar         = {:8.5f} (Prior probability of target user)".format(
+                cost_model["Ptar"]
+            )
+        )
+        print(
+            "   Pnon         = {:8.5f} (Prior probability of nontarget user)".format(
+                cost_model["Pnon"]
+            )
+        )
+        print(
+            "   Pspoof       = {:8.5f} (Prior probability of spoofing attack)".format(
+                cost_model["Pspoof"]
+            )
+        )
+        print(
+            "   Cfa_asv      = {:8.5f} (Cost of ASV falsely accepting a nontarget)".format(
+                cost_model["Cfa_asv"]
+            )
+        )
+        print(
+            "   Cmiss_asv    = {:8.5f} (Cost of ASV falsely rejecting target speaker)".format(
+                cost_model["Cmiss_asv"]
+            )
+        )
+        print(
+            "   Cfa_cm       = {:8.5f} (Cost of CM falsely passing a spoof to ASV system)".format(
+                cost_model["Cfa_cm"]
+            )
+        )
+        print(
+            "   Cmiss_cm     = {:8.5f} (Cost of CM falsely blocking target utterance which never reaches ASV)".format(
+                cost_model["Cmiss_cm"]
+            )
+        )
+        print(
+            "\n   Implied normalized t-DCF function (depends on t-DCF parameters and ASV errors), s=CM threshold)"
+        )
 
         if C2 == np.minimum(C1, C2):
-            print('   tDCF_norm(s) = {:8.5f} x Pmiss_cm(s) + Pfa_cm(s)\n'.format(C1 / C2))
+            print(
+                "   tDCF_norm(s) = {:8.5f} x Pmiss_cm(s) + Pfa_cm(s)\n".format(C1 / C2)
+            )
         else:
-            print('   tDCF_norm(s) = Pmiss_cm(s) + {:8.5f} x Pfa_cm(s)\n'.format(C2 / C1))
+            print(
+                "   tDCF_norm(s) = Pmiss_cm(s) + {:8.5f} x Pfa_cm(s)\n".format(C2 / C1)
+            )
 
     return tDCF_norm, CM_thresholds
